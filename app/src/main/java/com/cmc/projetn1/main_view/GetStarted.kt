d3b943aa26e2188db1108f4573319bf7c8941ab8
yaser <yaseryjjou@gmail.com>
2024-10-30 15:42:49 +0100

    Food.ma V2 with tabLayout

--- app/src/main/java/com/cmc/projetn1/main_view/GetStarted.kt
+++ app/src/main/java/com/cmc/projetn1/main_view/GetStarted.kt
@@ -2,11 +2,12 @@ package com.cmc.projetn1.main_view
 
 import android.os.Bundle
 import com.cmc.projetn1.ProjetN1Activity
+import com.cmc.projetn1.R
 import com.cmc.projetn1.databinding.GetStartedBinding
 import com.cmc.projetn1.adapters.GetstartedAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class GetStarted : ProjetN1Activity() {
-
     lateinit var binding: GetStartedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +17,18 @@ class GetStarted : ProjetN1Activity() {
 
         val adapter = GetstartedAdapter(this)
         binding.viewPager.adapter = adapter
-        binding.indicator.setViewPager(binding.viewPager)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager)
+        { tab, position ->
+            tab.text = when (position) {
+                0 -> "Présentation"
+                1 -> "Liste"
+                2 -> "Image"
+                else -> null
+            }
+
+        }.attach()
 
     }
 }
+
